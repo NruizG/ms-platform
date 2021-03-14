fcd7e88696fc2c639d8cf81c375133d2af80ffc5
Nicolas Ruiz Gallardo <nicolasruiz81@gmail.com>
2021-03-13 21:04:12 -0300

    ✨ Created controllers and services files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AccountsService } from './services/accounts/accounts.service';
+import { TransactionsService } from './services/transactions/transactions.service';
+import { TransactionsController } from './controllers/transactions/transactions.controller';
+import { AccountsController } from './controllers/accounts/accounts.controller';
+import { CustomersController } from './controllers/customers/customers.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    TransactionsController,
+    AccountsController,
+    CustomersController
+  ],
+  providers: [
+    AccountsService,
+    TransactionsService
+  ],
 })
 export class AppModule {}
